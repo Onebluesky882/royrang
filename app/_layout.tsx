ff462f61e19d8223bad2463905527b0f110f73bc
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-12-15 10:14:02 +0700

    gluestack setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Stack } from "expo-router";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 // export default function RootLayout() {
 //   return <Stack />;
 // }
 
 const RootLayout = () => {
-  return <Stack />;
+  return (
+    <GluestackUIProvider mode="light">
+      <Stack />
+    </GluestackUIProvider>
+  );
 };
 export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,18 @@
 import { FlatList } from "react-native";
 import products from "../assets/products.json";
 import ProductListItem from "../components/ProductListItem";
-
+import { Button, ButtonText } from "../components/ui/button";
 const HomeScreen = () => {
   return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item.name} />}
-    />
+    <>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item.name} />}
+      />
+      <Button>
+        <ButtonText>hello</ButtonText>
+      </Button>
+    </>
   );
 };
 export default HomeScreen;
