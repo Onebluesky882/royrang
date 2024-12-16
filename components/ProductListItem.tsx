48fff03a00c796c7ab4fa85d519469c0d7204a15
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-12-16 11:41:25 +0700

    style : remove button

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -47,19 +47,6 @@ const ProductListItem = ({
             </Heading>
             <Text size="sm">{description}</Text>
           </VStack>
-          <Box className="flex-col sm:flex-row">
-            <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-              <ButtonText size="sm">Add to cart</ButtonText>
-            </Button>
-            <Button
-              variant="outline"
-              className="px-4 py-2 border-outline-300 sm:flex-1"
-            >
-              <ButtonText size="sm" className="text-typography-600">
-                Wishlist
-              </ButtonText>
-            </Button>
-          </Box>
         </Card>
       </Pressable>
     </Link>
